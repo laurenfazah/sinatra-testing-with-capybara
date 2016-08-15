7e30f466db0634d9d5a69ff905933939a6ac213e
laurenfazah <laurenfazah@gmail.com>
2016-08-15 19:59:38 -0400

    Starter code for feature tests are in

--- test/features/user_sees_all_tasks_test.rb
+++ test/features/user_sees_all_tasks_test.rb
@@ -1,15 +1,6 @@
-require_relative "../test_helper"
+require_relative '../test_helper'
 
+# test to see all tasks
 class UserSeesAllTasksTest < FeatureTest
-include Capybara::DSL
-  def test_user_sees_all_tasks
-    task_manager.create({title: "Title1", description: "Description1"})
-    task_manager.create({title: "Title2", description: "Description2"})
-
-    visit '/tasks'
-save_and_open_page
-    assert page.has_content?("(1) Title1")
-    assert page.has_content?("(2) Title2")
-  end
-
+  include Capybara::DSL
 end
