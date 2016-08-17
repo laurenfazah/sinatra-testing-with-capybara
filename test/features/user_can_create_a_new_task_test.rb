553c03a8245505c0994261c44d47460b1c01a4e4
laurenfazah <laurenfazah@gmail.com>
2016-08-16 23:12:01 -0400

    Starter test methods in place for see and create tasks

--- test/features/user_can_create_a_new_task_test.rb
+++ test/features/user_can_create_a_new_task_test.rb
@@ -2,4 +2,6 @@ require_relative '../test_helper'
 
 # test to create new task
 class UserCanCreateANewTaskTest < FeatureTest
+  def test_user_creates_task
+  end
 end
--- test/features/user_sees_all_tasks_test.rb
+++ test/features/user_sees_all_tasks_test.rb
@@ -2,4 +2,6 @@ require_relative '../test_helper'
 
 # test to see all tasks
 class UserSeesAllTasksTest < FeatureTest
+  def test_user_sees_all_tasks
+  end
 end
